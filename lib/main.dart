69099e62ec23b8de2b352c168a73a2afe9d1f835
lmuniv00 <lmuniv00@fesb.hr>
2025-03-02 10:06:45 +0100

    Minor adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ void main() async {
     await Firebase.initializeApp(
         name: 'ble-app',
         options: FirebaseOptions(
-            apiKey: dotenv.env['FIREBASE_API_KEY']!,
-            authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
-            projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-            storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-            messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-            appId: dotenv.env['FIREBASE_APP_ID']!));
+            apiKey: "AIzaSyBbRv_zpx2gekxO9GeDpfm9whHsvZ2I5xk",
+            authDomain: "ble-attendance-4d245.firebaseapp.com",
+            projectId: "ble-attendance-4d245",
+            storageBucket: "ble-attendance-4d245.firebasestorage.app",
+            messagingSenderId: "57889017014",
+            appId: "1:57889017014:web:8002e88cff7ac9785a2a17"));
   }
 
   runApp(const MyApp());
